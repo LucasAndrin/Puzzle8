c3c30d0ddbc1fb39db3c0262f1856cad3cdc744b
Lucas Andrin <lucas.andrin.programmer@gmail.com>
2024-11-04 08:44:56 -0300

    feat: Configure TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts}',
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
   theme: {
     extend: {},
   },
+  safelist: [
+    {
+      pattern: /(top|left)-([0-9]+)/,
+    },
+  ],
   plugins: [],
-}
-
+};
